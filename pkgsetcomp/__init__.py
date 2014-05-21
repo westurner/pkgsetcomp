300dbcfa3176832cfe1f2ffacb7c038711e34e2e
Wes Turner <wes@wrd.nu>
2014-05-21 05:58:39 -0500

    BLD: set version number in VERSION.txt

--- pkgsetcomp/__init__.py
+++ pkgsetcomp/__init__.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+pkgsetcomp module
 
-__version__ = '0.1.2'
+"""
 
+def __read_version_txt():
+    import os.path
+    SETUPPY_PATH = os.path.abspath(
+        os.path.join(os.path.basename(__file__), '..'))
+    with open(os.path.join(SETUPPY_PATH, 'VERSION.txt')) as f:
+        version = f.read().strip()
+    return version
+
+
+global __version__
+global version
+version = __version__ = __read_version_txt()
+
+#__ALL__ = ['pkgsetcomp', 'version', '__version__']
